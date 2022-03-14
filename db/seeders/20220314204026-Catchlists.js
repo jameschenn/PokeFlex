b27d4d396792d942bbad1d38d811117130e6ee02
Khoi <kdng96@gmail.com>
2022-03-14 14:07:51 -0700

    set up seeders functions to insert seeders data

--- db/seeders/20220314204026-Catchlists.js
+++ db/seeders/20220314204026-Catchlists.js
@@ -2,25 +2,15 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+      return queryInterface.bulkInsert('Catchlists', [
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      ], {});
+
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      return queryInterface.bulkDelete('Catchlists', null, {});
+
   }
 };
