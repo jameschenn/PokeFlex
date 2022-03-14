eb28eff644515857def87de58f4911ccb6c534d7
Dayton Chen <daytonchen120@gmail.com>
2022-03-14 13:44:31 -0700

    db setup including migrations, models, and seeders file

--- db/seeders/20220314204006-Trainers.js
+++ db/seeders/20220314204006-Trainers.js
@@ -3,11 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
+            return queryInterface.bulkInsert('People', [{
         name: 'John Doe',
         isBetaMember: false
       }], {});
