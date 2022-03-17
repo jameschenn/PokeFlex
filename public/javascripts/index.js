cee8aa1c9439f766af71c0e63c6352f6e0abcc95
grant <grntwalton@gmail.com>
2022-03-17 14:37:56 -0700

    fixed with css

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,10 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
-    
+    const form = document.querySelector('#form');
+    const option = document.querySelector('.formSelector');
+    const formButton = document.querySelector('#formButton');
+    formButton.addEventListener('click', e => {
+        form.action += `/${option.id}`;
+        console.log("hello")
+    })
 })
