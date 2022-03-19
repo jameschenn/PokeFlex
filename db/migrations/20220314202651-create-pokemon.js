8d358c5b3359bbf5746c710ed6a609ac0071d3b6
James Chen <jameschen09@yahoo.com>
2022-03-19 14:50:28 -0700

    added biology seed data up to machamp

--- db/migrations/20220314202651-create-pokemon.js
+++ db/migrations/20220314202651-create-pokemon.js
@@ -24,6 +24,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING(50)
       },
+      biology: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
